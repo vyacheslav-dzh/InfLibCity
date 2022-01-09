6c91e5d2fade8786929055f9a7ba2f166f6627bd
iokarch <ivankarch2020@gmail.com>
2022-01-09 14:20:00 +0300

    added subscription class

---
+++ InfLibCity/Subscription.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfLibCity {
+    class Subscription {
+
+        public int id;
+        public int readerId;
+        public int subjectId;
+        public string startDate;
+        public string finishDate;
+        public bool isActive;
+
+        public Subscription() {
+            this.id = -1;
+            this.readerId = -1;
+            this.subjectId = -1;
+            this.startDate = "";
+            this.finishDate = "";
+            this.isActive = false;
+        }
+
+
+        public Subscription(int id, int readerId, int subjectId, string startDate, string finishDate, bool isActive) {
+            this.id = id;
+            this.readerId = readerId;
+            this.subjectId = subjectId;
+            this.startDate = startDate;
+            this.finishDate = finishDate;
+            this.isActive = isActive;
+        }
+
+    }
+}
