381bb6000f664754dd7c0cc23c9dd97a866c4935
iokarch <ivankarch2020@gmail.com>
2022-01-10 01:52:16 +0300

    ffff

--- InfLibCity/DBManipulator.cs
+++ InfLibCity/DBManipulator.cs
@@ -114,6 +114,25 @@ namespace InfLibCity
         public static Person getPerson(int person_id)
         {
 
+            using (MySqlConnection conn = new MySqlConnection(connectionString)) {
+
+
+                DataSet dataSet = new DataSet();
+                string command = $"SELECT *  FROM Peoples where people_user_id = {person_id}";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command, conn);
+                adapter.Fill(dataSet);
+
+                var stroka = dataSet.Tables[0].Select()[0];
+
+                People people = new People((int)stroka[0],
+                                           (int)stroka[1],
+                                           stroka[2].ToString(),
+                                           stroka[3].ToString(),
+                                           stroka[4].ToString());
+
+                return people;
+
+            }
         }
 
 
